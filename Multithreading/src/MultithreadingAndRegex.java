dc006a54ff658cae421be557e282d016c311243e
KUNLIN YU <3238127737@qq.com>
2021-11-28 16:13:48 +0800

    添加了注释

--- Multithreading/src/MultithreadingAndRegex.java
+++ Multithreading/src/MultithreadingAndRegex.java
@@ -11,6 +11,7 @@ public class MultithreadingAndRegex {
         int[] b={2,4,6,8,10};
         multithread(a,b);
     }
+    //多线程组还在理解，之前没有学过，下面这段代码来自CSDN。
     public static void multithread(int[] a,int[] b){
         Object object = new Object();
         Thread thread1 = new Thread(()  -> {
